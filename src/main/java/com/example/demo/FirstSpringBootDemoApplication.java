2fbb618453154d107a3b0a5918a80320a670df13
zhuhaitao <zhuhaitao@sishuxuefu.com>
2018-12-21 17:14:28 +0800

    启动入口

--- src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
+++ src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+/**
+ * 启动入口*/
 @SpringBootApplication
 @Configuration
 public class FirstSpringBootDemoApplication implements WebMvcConfigurer {
