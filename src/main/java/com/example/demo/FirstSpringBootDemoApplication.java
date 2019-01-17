b0bc05f258e36ab606c0b3700ebc33d36676ffd3
mnizht <mnizht@qq.com>
2019-01-17 23:19:05 +0800

    update aplication

--- src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
+++ src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * 启动入口*/
 @SpringBootApplication
 @Configuration
 public class FirstSpringBootDemoApplication implements WebMvcConfigurer {
