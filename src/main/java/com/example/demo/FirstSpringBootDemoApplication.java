6311511bf443e0904b22cb7e305aaa7e278093ef
mnizht <mnizht@qq.com>
2019-02-08 11:34:45 +0800

    no change

--- src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
+++ src/main/java/com/example/demo/FirstSpringBootDemoApplication.java
@@ -21,5 +21,4 @@ public class FirstSpringBootDemoApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBootDemoApplication.class, args);
 	}
-
 }
