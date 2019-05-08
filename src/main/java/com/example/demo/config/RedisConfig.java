93e319f03e89fa14d46aef9d4cdb63c6c10073e9
mnizht <mnizht@qq.com>
2019-05-08 18:28:43 +0800

    添加Redis注释

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -40,11 +40,19 @@ public class RedisConfig {
     return factory;
   }
 
+  /**
+   * 字符串序列化器
+   * Redis是基于字符串存储的Nosql，而java是基于对象的语言，对象是无法存储到Redis中的，
+   * Java在向Redis中写数据时，需要先将数据序列化成二进制字符串才能保存。取数据时也要反序列化后才能正常显示。
+   * @return
+   */
   @Bean(name="redisTemplate")
   public RedisTemplate<Object, Object> initRedisTemplate() {
     RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(initRedisConnectionFactory());
+    //RedisTemplate 会自动初始化 StringRedisSerializer,所以这里直接获取
     RedisSerializer<String> stringRedisSerializer = redisTemplate.getStringSerializer();
+    //设置字符串序列化器，这样Spring就会把Redis的key当做字符串处理了
     redisTemplate.setKeySerializer(stringRedisSerializer);
     redisTemplate.setHashKeySerializer(stringRedisSerializer);
     redisTemplate.setHashValueSerializer(stringRedisSerializer);
